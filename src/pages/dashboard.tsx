8bcbc7097b47231d47dbb070648c2072fc45a89a
PedroItaloQ <peedroitalo2906@gmail.com>
2024-10-02 18:20:05 -0300

    feat: Created Layout dashbard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+// pages/dashboard.tsx
+import MainLayout from '../layouts/MainLayout';
+
+const Dashboard = () => {
+  return (
+    <MainLayout>
+      <h2 className="text-2xl font-bold">Dashboard</h2>
+      {/* Conteúdo do dashboard */}
+    </MainLayout>
+  );
+};
+
+export default Dashboard;
